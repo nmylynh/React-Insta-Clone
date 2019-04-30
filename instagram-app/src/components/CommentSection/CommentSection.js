25ef5ea70810c02d3ea9a7254003ee6a12479418
Moses Samuel <mosamuel98@gmail.com>
2019-04-30 16:08:55 -0700

    introduce local state to CommentSectoin

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./CommentSection.css"
 
-function CommentSection(props) {
-    return ( 
+class CommentSection extends Component {
+    state = {
+        comments: [],
+        value: ""
+    }
+
+    handleChange = (e) => {
+        this.setState({value: e.target.textContent})
+    }
+    
+    render() {
+        return ( 
             <div className="comment-section"> 
-                {props.comments.map(renderComment)} 
-                <span className="timestamp">{props.timestamp || Date.now()}</span> {/* I removed timestamp prop, so Date.now is temporary default */}
+                {this.props.comments.map(renderComment)} 
+                <span className="timestamp">{this.props.timestamp || Date.now()}</span> {/* I removed timestamp prop, so Date.now is temporary default */}
                 <hr/>
-                <input className="add-comment" placeholder="Add a comment..." />
+                <input onChange={this.handleChange} className="add-comment"  ref="input" placeholder="Add a comment..." />
+
+                
+                <div style={{color:'red', border: 'red'}}>{this.state.value}</div>
             </div> 
         )
+    }
 }
 
 function renderComment(props, index){
-    console.log(props)
     return (
         <div key={index*100} className="comment">
             <span className="comment-user-name">{props.username}</span>
