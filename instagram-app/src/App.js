cac7d7322fe84a50e66a4003e521a30d2dc1d959
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-30 13:42:25 -0700

    refactor: changed the folder structure of components

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import SearchBar from './components/SearchBar';
+import SearchBar from './components/SearchBar/SearchBar';
 import dummyData from '../src/dummy-data';
-import PostGrid from './components/PostGrid';
+import PostGrid from './components/PostGrid/PostGrid';
 
 class App extends React.Component {
   state = dummyData
---
+++ instagram-app/src/components/PostGrid/PostGrid.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import "./PostContainer.css";
+import CommentSection from "../CommentSection/CommentSection";
+
+export default function PostGrid(props){
+    return props.posts.map(renderPostContainer)
+}
+
+
+function renderPostContainer(props, index){
+    return (
+            <div key={index} className="card">
+                <div className="post-header">
+                    <img src={props.thumbnailUrl} className="thumbnail-img" alt="thumbnail"/>
+                    <div className="users-name"> {props.username} </div> 
+                </div>
+                <img src={props.imageUrl} className="post-image" alt="post" />     
+                <div className="user-interaction">
+                    <i className="far fa-heart"></i>
+                    <i className="far fa-comment"></i>
+                </div>
+                <p className="likes">{props.likes} likes</p>
+
+
+                <CommentSection
+                comments={props.comments}
+                timestamp={props.timestamp}
+                />
+
+            </div>
+        )
+}
+
+PostGrid.propTypes = {
+    username: PropTypes.string,
+    thumbnailUrl: PropTypes.string,
+    imageUrl: PropTypes.string,
+    likes: PropTypes.number,
+    timestamp: PropTypes.string
+}
