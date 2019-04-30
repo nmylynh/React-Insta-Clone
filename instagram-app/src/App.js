c8cf4e93d130517c4ed06af258e27428c25e483c
Moses Samuel <mosamuel98@gmail.com>
2019-04-30 14:49:33 -0700

    feature: state gets updated when component mounts

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -4,20 +4,23 @@ import SearchBar from './components/SearchBar/SearchBar';
 import dummyData from '../src/dummy-data';
 import PostGrid from './components/PostGrid/PostGrid';
 
-class App extends React.Component {
-  state = dummyData
+export default class App extends React.Component {
+  state = {
+    posts: []
+  }
+  
+  componentDidMount() {    
+    this.setState({posts : dummyData})
+  }
+  
+  render() {
+    return (
+      <div className="App">
 
-  render(){
-  return (
-    <div className="App">
+        <SearchBar />
 
-      <SearchBar />
-
-      <PostGrid posts = {this.state} />
-
-    </div>
-  );
+        <PostGrid posts = {this.state.posts} />
+      </div>
+    )
+  }
 }
-}
-
-export default App;
