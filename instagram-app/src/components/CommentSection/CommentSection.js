807b7fae1df18c9723ed269423dab8c0a055b779
Moses Samuel <mosamuel98@gmail.com>
2019-04-30 22:02:34 -0700

    wip: attempt to enable user to add new comments

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -8,6 +8,23 @@ class CommentSection extends Component {
         value: ""
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault()
+        this.setState((prevState) => {
+            console.log('state:', this.state)
+            console.log('prevState', prevState)
+            return {
+                ...prevState,
+                comments: prevState.comments.concat({
+                    username: this.props.username,
+                    text: 'why isnt this working?',
+                    createdAt: Date.now()
+                })
+            }})
+
+        setTimeout(() => console.log(this.state.comments), 3000)
+    }
+
     handleChange = (e) => {
         this.setState({value: e.target.textContent})
     }
@@ -18,7 +35,10 @@ class CommentSection extends Component {
                 {this.props.comments.map(renderComment)} 
                 <span className="timestamp">{this.props.timestamp || Date.now()}</span> {/* I removed timestamp prop, so Date.now is temporary default */}
                 <hr/>
-                <input onChange={this.handleChange} className="add-comment"  ref="input" placeholder="Add a comment..." />
+
+                <form onSubmit={this.handleSubmit}>
+                    <input onChange={this.handleChange} className="add-comment" placeholder="Add a comment..." />
+                </form>
 
                 
                 <div style={{color:'red', border: 'red'}}>{this.state.value}</div>
